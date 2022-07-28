a96fad5816e38ec0c27cd58f856209a64d61784e
bunhor-deb <94274082+bunhor-deb@users.noreply.github.com>
2022-07-28 18:37:39 +0700

    import flask package

--- models/Teacher.py
+++ models/Teacher.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+# from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 from passlib.handlers import mysql
 from ruamel import yaml
