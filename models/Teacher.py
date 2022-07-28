ce8be7cb24c3073e31563004f2bc18a0c90b0156
bunhor-deb <94274082+bunhor-deb@users.noreply.github.com>
2022-07-28 23:19:45 +0700

    config data base in Teacher class but not work

--- models/Teacher.py
+++ models/Teacher.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-# from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 from passlib.handlers import mysql
 from ruamel import yaml
