b45e3e2ab9b856de5fa72199dc0b16aa162992c8
bunhor-deb <bunhor0303@gmail.com>
2022-08-03 10:47:16 -0700

    config for deploy to HEROKU

--- users_management_api/__init__.py
+++ users_management_api/__init__.py
@@ -2,3 +2,5 @@ from users_management_api.controller.TeacherController import *
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
+
--- users_management_api/controller/TeacherController.py
+++ users_management_api/controller/TeacherController.py
@@ -3,11 +3,11 @@ from users_management_api.service.TeacherService import *
 
 @app.route("/allTeacher", methods=["GET"])
 def getAllTeacher():
-    teachers = getAllRecords()
-    lists = []
-    for teacher in teachers:
-        lists.append(teacher.toDict())
-        return jsonify({"CODE": "000", "MESSAGE": "OK This is Python API"})
+    # teachers = getAllRecords()
+    # lists = []
+    # for teacher in teachers:
+    #     lists.append(teacher.toDict())
+    return jsonify({"CODE": "000", "MESSAGE": "OK This is Python API"})
 
 
 
